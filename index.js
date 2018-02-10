f54305fbbcf30a0ff6f091febe393c99ebdcaada
Philipp Jahn <calyphornia@techno4ever.fm>
2018-02-10 02:01:45 +0100

    mysql

--- index.js
+++ index.js
@@ -6,11 +6,15 @@ var mysql = require('mysql');
 var con = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE
 });
 
 con.connect(function(err) {
-  if (err) throw err;
+  if (err) {
+    console.log("Error");
+    return;
+  }
   console.log("Connected!");
 });
 
